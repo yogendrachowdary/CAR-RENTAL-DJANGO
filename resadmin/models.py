e62efaa1d9370736b07fa5681df1aeb86e41cd10
yogendrachowdary <yogendrachowdary101@gmail.com>
2023-10-01 18:38:14 +0530

    implemented foreign key

--- resadmin/models.py
+++ resadmin/models.py
@@ -37,6 +37,27 @@ class Customer(models.Model):
     #     return str(self.customerid)
     
 
+
+class Owner(models.Model):
+    ownerid = models.BigIntegerField(primary_key=True,unique=True)
+    city = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    gender_choices = (("male", "MALE"), ("female", "FEMALE"))
+    gender = models.CharField(max_length=10, blank=False, choices=gender_choices)
+    age = models.CharField(max_length=100, blank=False)
+    password = models.CharField(max_length=150, blank=False, default="12345")
+    email = models.CharField(max_length=120, blank=False, unique=True)
+    contact = models.CharField(max_length=20, blank=False, unique=True)
+
+
+    class Meta:
+        db_table = "owner_table"
+    
+    def __str__(self):
+        return str(self.ownerid)
+
+
+
 class Car(models.Model):
     id=models.AutoField(primary_key=True)
     model=models.CharField(max_length=100)
@@ -47,30 +68,15 @@ class Car(models.Model):
     year = models.PositiveIntegerField(blank=False)  # Example: Year of the car
     price = models.CharField(max_length=15,blank=False)  # Example: Price of the car
     manufacturer = models.CharField(max_length=100,blank=False)  # Example: Car manufacturer
+    ownerid=models.ForeignKey(Owner,on_delete=models.CASCADE)
 
     class Meta:
         db_table="car_table"
     
     def __str__(self):
-        return self.model
+        return str(self.model)
 
 
 
-class Owner(models.Model):
-    ownerid = models.AutoField(primary_key=True)
-    city = models.CharField(max_length=100, blank=False)
-    name = models.CharField(max_length=100, blank=False)
-    gender_choices = (("male", "MALE"), ("female", "FEMALE"))
-    gender = models.CharField(max_length=10, blank=False, choices=gender_choices)
-    age = models.CharField(max_length=100, blank=False)
-    password = models.CharField(max_length=150, blank=False, default="12345")
-    email = models.CharField(max_length=120, blank=False, unique=True)
-    contact = models.CharField(max_length=20, blank=False, unique=True)
-
-    class Meta:
-        db_table = "owner_table"
-    
-    def __str__(self):
-        return self.name
 
 
